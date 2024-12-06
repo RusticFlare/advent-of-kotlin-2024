80098680640fd10b58708b9054f467ab1b49f38f
rusticflare <j_d_baker@hotmail.com>
2024-12-06 15:35:00 +0000

    Day03 Part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,3 +1,4 @@
+import NumberMultiplierOperation.*
 import io.kotest.assertions.withClue
 import io.kotest.matchers.shouldBe
 
@@ -6,27 +7,94 @@ fun main() {
         return input.sumOf { it.multiplySomeNumbers() }
     }
 
-//    fun part2(input: List<String>): Int {
-//        return 0
-//    }
-
-    val testInput = readInput("Day03_test")
-    withClue("part1") {
-        part1(testInput) shouldBe 161u
+    fun part2(input: List<String>): ULong {
+        return input.joinToString(separator = "").doOrDoNotMultiplySomeNumbers()
     }
-//    withClue("part2") {
-//        part2(testInput) shouldBe 4
-//    }
+
+    val testInput1 = readInput("Day03_test1")
+    withClue("part1") {
+        part1(testInput1) shouldBe 161u
+    }
+    val testInput2 = readInput("Day03_test2")
+    withClue("part2") {
+        part2(testInput2) shouldBe 48u
+    }
 
     val input = readInput("Day03")
     "Day 3".println()
     "  Part 1: ${part1(input)}".println()
-//    "  Part 2: ${part2(input)}".println()
+    "  Part 2: ${part2(input)}".println()
 }
 
-private val regex = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)")
+private val mulRegex = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)")
+private val doRegex = Regex("do\\(\\)")
+private val doNotRegex = Regex("don't\\(\\)")
 
-private fun String.multiplySomeNumbers() = regex.findAll(this)
-    .map { match -> match.groupValues.drop(1).map { it.toULong() }.reduce(ULong::times) }
-    .sum()
+private fun String.multiplySomeNumbers() = mulRegex.findAll(this)
+    .sumOf { match -> match.groupValues.drop(1).map { it.toULong() }.reduce(ULong::times) }
 
+private fun String.doOrDoNotMultiplySomeNumbers() = numberMultiplierOperations()
+    .fold(NumberMultiplierState.initial(), NumberMultiplierState::transition).value
+
+private fun String.numberMultiplierOperations(): Sequence<NumberMultiplierOperation> {
+    val muls = mulRegex.findAll(this)
+        .map { match -> Mul(match.range, match.groupValues.drop(1).map { it.toULong() }.reduce(ULong::times)) }
+
+    val dos = doRegex.findAll(this)
+        .map { Do(it.range) }
+
+    val doNots = doNotRegex.findAll(this)
+        .map { DoNot(it.range) }
+
+    return (muls + dos + doNots).sortedBy { it.range.first }
+}
+
+private sealed interface NumberMultiplierOperation{
+
+    val range: IntRange
+
+    data class Mul(
+        override val range: IntRange,
+        val result: ULong,
+    ) : NumberMultiplierOperation
+
+    data class Do(
+        override val range: IntRange,
+    ) : NumberMultiplierOperation
+
+    data class DoNot(
+        override val range: IntRange,
+    ) : NumberMultiplierOperation
+}
+
+private sealed interface NumberMultiplierState {
+
+    val value: ULong
+
+    fun transition(operation: NumberMultiplierOperation): NumberMultiplierState
+
+    data class Doing(
+        override val value: ULong
+    ) : NumberMultiplierState {
+
+        override fun transition(operation: NumberMultiplierOperation) = when(operation) {
+            is Do -> this
+            is DoNot -> NotDoing(value)
+            is Mul -> copy(value = value + operation.result)
+        }
+    }
+
+    data class NotDoing(
+        override val value: ULong
+    ) : NumberMultiplierState {
+
+        override fun transition(operation: NumberMultiplierOperation) = when(operation) {
+            is Do -> Doing(value)
+            is DoNot, is Mul -> this
+        }
+    }
+
+    companion object {
+        fun initial(): NumberMultiplierState = Doing(value = 0u)
+    }
+}
